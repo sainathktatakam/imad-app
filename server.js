2ef34c4431f52536ffbf483b58f3c23e1f612335
SAINATH KATAKAM <sainath.katakam@gmail.com>
2017-08-18 11:45:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send('My first Article');
 });
 
 app.get('/ui/style.css', function (req, res) {

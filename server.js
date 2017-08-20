78e95b3cb09b3b6e69b0713a18ccf29388f6f958
SAINATH KATAKAM <sainath.katakam@gmail.com>
2017-08-20 12:50:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ function createHTMLtemplate(data){
     <head>
         <title>${Title}; </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+        
         
     </head>
     
